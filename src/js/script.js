ee5ffdd899908927ba30d950caea14b99a0b320e
Maria Ferrari <maria.ferrari@kenzie.com.br>
2022-03-30 14:40:04 -0300

    feat: estilização do modal feita

--- src/js/script.js
+++ src/js/script.js
@@ -15,4 +15,36 @@ function criarTabuleiro(input){
     main.appendChild(container)
 }
 
-criarTabuleiro()
+function criarModal() {
+    const main = document.querySelector('main')
+    const popUp = document.createElement('div');
+    popUp.setAttribute('id', 'popUp')
+    popUp.classList.add('show')
+    main.appendChild(popUp)
+
+    const popUpTitulo = document.createElement('h2')
+    popUpTitulo.setAttribute('id', 'popUp_title')
+    popUpTitulo.innerText = 'JOGO GENIUS'
+    popUp.appendChild(popUpTitulo)
+
+    const form = document.createElement('form')
+    form.setAttribute('id', 'popUp_form')
+
+    const input = document.createElement('input')
+    input.id = 'input_name'
+
+    const label = document.createElement('label')
+    label.htmlFor = 'input_name';
+    label.innerText = 'Insira seu nome aqui:';
+
+    const button = document.createElement('button')
+    button.setAttribute('id', 'popUp_button')
+    button.innerText = 'Iniciar o jogo!';
+
+    form.appendChild(label)
+    form.appendChild(input)
+    form.appendChild(button)
+    popUp.appendChild(form)
+
+}
+criarModal()
