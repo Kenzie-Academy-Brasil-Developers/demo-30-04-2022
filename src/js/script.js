f104a3bd114c7406f83d7a40c0baa496a8a4e7ba
Jardel Kenzie <jardel@kenzie.com.br>
2022-03-31 14:16:08 -0300

    fix: subindo modal

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,20 @@
+function criarTabuleiro(input){
+    const main = document.getElementById('main')
+
+    const container = document.createElement('section')
+    container.classList.add('tabuleiro')
+
+    container.innerHTML = `
+        <div class="botao botao--green"></div>
+        <div class="botao botao--yellow"></div>
+        <div class="botao botao--red"></div>
+        <div class="botao botao--blue"></div>
+        <div class="infos">${input}</div>
+    `
+
+    main.appendChild(container)
+}
+
 function criarModal() {
     const main = document.querySelector('main')
     const popUp = document.createElement('div');
